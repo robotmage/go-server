fcb6a417ab984d4eabf1f96dc98cea642f8a3b28
robotmage <88262066+robotmage@users.noreply.github.com>
2021-08-01 11:05:26 -0500

    add version

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"os"
 )
 
+const version = "0.1"
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	hostName, _ := os.Hostname()
-	fmt.Fprintf(w, "Hi There from [%s], I love %s!\n", hostName, r.URL.Path[1:])
+	fmt.Fprintf(w, "Version %s Hi There from [%s], I love %s!\n",version, hostName, r.URL.Path[1:])
 }
 
 func main() {
